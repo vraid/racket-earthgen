a5bf99220c1d35dfc44ea1b4c7415e129edf9660
Tobias Grelsson <tgrelsson@gmail.com>
2013-10-20 20:33:28 +0200

    more concise

--- parallel-util.rkt
+++ parallel-util.rkt
@@ -5,8 +5,10 @@
 (provide vector-map!-parallel)
 
 (define (vector-map!-parallel fn vec)
-  (define fs (for/vector ([n (build-vector (vector-length vec) identity)])
-               (future (lambda () (vector-set! vec n (fn (vector-ref vec n)))))))
+  (define fs (build-vector
+              (vector-length vec)
+              (lambda (n)
+                (future (lambda () (vector-set! vec n (fn (vector-ref vec n))))))))
   (begin
     (for/vector ([f fs]) (touch f))
     vec))
