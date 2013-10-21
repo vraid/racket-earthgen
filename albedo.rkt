96be7c2ba4cde80d3dac163443a39d1082d0e430
Tobias Grelsson <tgrelsson@gmail.com>
2013-10-21 23:35:30 +0200

    albedo

---
+++ albedo.rkt
@@ -0,0 +1,39 @@
+#lang racket
+
+(require "planet-tile-struct.rkt")
+(provide planet-tile-albedo)
+
+(define cloud-albedo 0.8)
+(define water-albedo 0.1)
+(define water-ice-albedo 0.6)
+(define snow-albedo 0.8)
+(define desert-albedo 0.4)
+(define forest-albedo 0.15)
+(define grassland-albedo 0.25)
+
+(define (interpolate a b d)
+  (fl+ (fl* a (fl- 1.0 d))
+       (fl* b d)))
+
+(define (water-surface-albedo tile)
+  (interpolate water-albedo
+               water-ice-albedo
+               (planet-tile-ice-cover tile)))
+
+(define (land-surface-albedo tile)
+  (interpolate desert-albedo
+               forest-albedo
+               (planet-tile-vegetation-cover tile)))
+
+(define (surface-albedo tile)
+  (if (planet-tile-water? tile)
+      (water-surface-albedo tile)
+      (land-surface-albedo tile)))
+
+(define (sky-albedo tile)
+  (fl* cloud-albedo
+       (planet-tile-cloud-cover tile)))
+
+(define (planet-tile-albedo tile)
+  (fl- 1.0 (fl* (fl- 1.0 (surface-albedo tile))
+                (fl- 1.0 (sky-albedo tile)))))
