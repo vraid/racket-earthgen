45e5857436cf1251abdea7e768a3f55bc522e88e
Tobias Grelsson <tgrelsson@gmail.com>
2013-12-01 23:56:28 +0100

    orbit and sunlight functions

---
+++ orbit.rkt
@@ -0,0 +1,16 @@
+#lang typed/racket
+
+(require "constants.rkt")
+
+(provide solar-equator
+         solar-irradiance)
+
+(: solar-equator (Flonum Flonum -> Flonum))
+(define (solar-equator axial-tilt time-of-year)
+  (* axial-tilt
+     (sin (* time-of-year tau))))
+
+(: solar-irradiance (Flonum Flonum -> Flonum))
+(define (solar-irradiance solar-equator latitude)
+  (max 0.0
+       (cos (- solar-equator latitude))))
