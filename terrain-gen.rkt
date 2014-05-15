c864c8b9b2bd4bc779d450d63f843b59aafe30f7
Tobias Grelsson <tgrelsson@gmail.com>
2014-05-15 23:26:07 +0200

    default terrain shouldn't be tiny

--- terrain-gen.rkt
+++ terrain-gen.rkt
@@ -1,5 +1,5 @@
 (values
- 3
+ 6
  (let* ([continent (heightmap-map
                     (lambda (a)
                       (if (< 0 a)
@@ -9,7 +9,7 @@
                      250.0
                      (heightmap-create
                       (heightmap-parameters/kw
-                       #:seed "dzig"
+                       #:seed "vazhno"
                        #:base-level 2
                        #:amplitude 800.0
                        #:persistence 0.65))))]
@@ -58,30 +58,7 @@
                            #:persistence 0.7))))]
         [mountain (heightmap-combine
                    continent
-                   mountain-base)]
-        [trench-base (heightmap-map*
-                      (lambda (a b . ns)
-                        (if (both
-                             true?
-                             (> -50.0 a)
-                             (> 0.0 b))
-                            b
-                            0.0))
-                      mountain
-                      (heightmap-raise
-                       1500.0
-                       (heightmap-create
-                        (heightmap-parameters/kw
-                         #:seed "trench"
-                         #:base-level 3
-                         #:amplitude 2500.0
-                         #:persistence 0.7))))]
-        [final-terrain
-         (heightmap-combine
-          mountain
-          trench-base)])
-;   final-terrain
-
+                   mountain-base)])
    (heightmap-combine
     continent
  snake-mountain)))
