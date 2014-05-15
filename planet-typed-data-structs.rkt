4c4523074c23334b58c2049d8471480d4868cf15
Tobias Grelsson <tgrelsson@gmail.com>
2014-05-15 23:59:00 +0200

    require/typed/provide

--- planet-typed-data-structs.rkt
+++ planet-typed-data-structs.rkt
@@ -1,12 +1,5 @@
 #lang typed/racket
 
-(provide (struct-out tile-data)
-         (struct-out corner-data)
-         (struct-out edge-data)
-         make-tile-data
-         make-corner-data
-         make-edge-data)
-
 (require "types.rkt")
 
 (define-type boolean-access (index -> Boolean))
@@ -16,42 +9,42 @@
 (define-type flonum-access (index -> Flonum))
 (define-type flonum-set! (index Flonum -> Void))
 
-(require/typed "planet-data-structs.rkt"
-               [#:struct tile-data
-                         ([elevation : flonum-access]
-                          [water-level : flonum-access]
-                          [sunlight : flonum-access]
-                          [temperature : flonum-access]
-                          [humidity : flonum-access]
-                          [precipitation : flonum-access]
-                          [snow-cover : flonum-access]
-                          [vegetation : flonum-access]
-                          [vertical-air-flow : flonum-access]
-                          [tropopause-altitude : flonum-access]
-                          [elevation-set! : flonum-set!]
-                          [water-level-set! : flonum-set!]
-                          [temperature-set! : flonum-set!]
-                          [sunlight-set! : flonum-set!]
-                          [humidity-set! : flonum-set!]
-                          [precipitation-set! : flonum-set!]
-                          [snow-cover-set! : flonum-set!]
-                          [vegetation-set! : flonum-set!]
-                          [vertical-air-flow-set! : flonum-set!]
-                          [tropopause-altitude-set! : flonum-set!])]
-               [#:struct corner-data
-                         ([elevation : flonum-access]
-                          [river-direction : fixnum-access]
-                          [elevation-set! : flonum-set!]
-                          [river-direction-set! : fixnum-set!])]
-               [#:struct edge-data
-                         ([has-river? : boolean-access]
-                          [river-flow : flonum-access]
-                          [surface-air-flow : flonum-access]
-                          [troposphere-air-flow : flonum-access]
-                          [has-river?-set! : boolean-set!]
-                          [river-flow-set! : flonum-set!]
-                          [surface-air-flow-set! : flonum-set!]
-                          [troposphere-air-flow-set! : flonum-set!])]
-               [make-tile-data (Integer -> tile-data)]
-               [make-corner-data (Integer -> corner-data)]
-               [make-edge-data (Integer -> edge-data)])
+(require/typed/provide "planet-data-structs.rkt"
+                       [#:struct tile-data
+                                 ([elevation : flonum-access]
+                                  [water-level : flonum-access]
+                                  [sunlight : flonum-access]
+                                  [temperature : flonum-access]
+                                  [humidity : flonum-access]
+                                  [precipitation : flonum-access]
+                                  [snow-cover : flonum-access]
+                                  [vegetation : flonum-access]
+                                  [vertical-air-flow : flonum-access]
+                                  [tropopause-altitude : flonum-access]
+                                  [elevation-set! : flonum-set!]
+                                  [water-level-set! : flonum-set!]
+                                  [temperature-set! : flonum-set!]
+                                  [sunlight-set! : flonum-set!]
+                                  [humidity-set! : flonum-set!]
+                                  [precipitation-set! : flonum-set!]
+                                  [snow-cover-set! : flonum-set!]
+                                  [vegetation-set! : flonum-set!]
+                                  [vertical-air-flow-set! : flonum-set!]
+                                  [tropopause-altitude-set! : flonum-set!])]
+                       [#:struct corner-data
+                                 ([elevation : flonum-access]
+                                  [river-direction : fixnum-access]
+                                  [elevation-set! : flonum-set!]
+                                  [river-direction-set! : fixnum-set!])]
+                       [#:struct edge-data
+                                 ([has-river? : boolean-access]
+                                  [river-flow : flonum-access]
+                                  [surface-air-flow : flonum-access]
+                                  [troposphere-air-flow : flonum-access]
+                                  [has-river?-set! : boolean-set!]
+                                  [river-flow-set! : flonum-set!]
+                                  [surface-air-flow-set! : flonum-set!]
+                                  [troposphere-air-flow-set! : flonum-set!])]
+                       [make-tile-data (Integer -> tile-data)]
+                       [make-corner-data (Integer -> corner-data)]
+                       [make-edge-data (Integer -> edge-data)])
