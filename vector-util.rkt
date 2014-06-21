0728eceb422e61c4d8233ed23ef43c79f73e3f30
Tobias Grelsson <tgrelsson@gmail.com>
2014-06-21 16:22:45 +0200

    vector building

--- vector-util.rkt
+++ vector-util.rkt
@@ -1,8 +1,9 @@
 #lang typed/racket
 
-(provide vector-index)
+(provide (all-defined-out))
 
-(require "types.rkt")
+(require "types.rkt"
+         math/flonum)
 
 (: vector-index (All (A) (A (Vectorof A) -> index)))
 (define (vector-index e v)
@@ -10,3 +11,15 @@
     (if (false? m)
         (vector-length v)
         m)))
+
+(: build-vector-ref (All (A) (Natural (integer -> A) -> (integer -> A))))
+(define (build-vector-ref count f)
+  (let ([v (build-vector count f)])
+    (lambda: ([n : integer])
+      (vector-ref v n))))
+
+(: build-flvector-ref (Natural (integer -> Flonum) -> (integer -> Flonum)))
+(define (build-flvector-ref count f)
+  (let ([v (build-flvector count f)])
+    (lambda: ([n : integer])
+      (flvector-ref v n))))
