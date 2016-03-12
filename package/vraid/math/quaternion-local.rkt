828f69706369b953b935cf379782bbe09ed45cf5
vraid <vraid@radish>
2016-03-12 23:18:05 +0100

    cleaned up quaternion files

--- package/vraid/math/quaternion-local.rkt
+++ package/vraid/math/quaternion-local.rkt
@@ -9,16 +9,82 @@
 (define-type quaternion-index-vector (Vectorof quaternion-index))
 
 (struct quaternion
-  ([a : Flonum]
-   [i : Flonum]
-   [j : Flonum]
-   [k : Flonum])
+  ([a : Float]
+   [i : Float]
+   [j : Float]
+   [k : Float])
   #:transparent)
 
 (define a quaternion-a)
 (define i quaternion-i)
 (define j quaternion-j)
 (define k quaternion-k)
+
+(: fl->vector->quaternion (Float FlVector -> quaternion))
+(define (fl->vector->quaternion f v)
+  (quaternion
+   f
+   (flvector-ref v 0)
+   (flvector-ref v 1)
+   (flvector-ref v 2)))
+
+(: vector->quaternion (FlVector -> quaternion))
+(define (vector->quaternion v)
+  (fl->vector->quaternion 0.0 v))
+
+(: quaternion-map ((Float -> Float) quaternion -> quaternion))
+(define (quaternion-map f q)
+  (quaternion (f (a q))
+              (f (i q))
+              (f (j q))
+              (f (k q))))
+
+(: quaternion-map2 ((Float Float -> Float) quaternion quaternion -> quaternion))
+(define (quaternion-map2 f q r)
+  (quaternion (f (a q) (a r))
+              (f (i q) (i r))
+              (f (j q) (j r))
+              (f (k q) (k r))))
+
+(: quaternion-scale (quaternion Float -> quaternion))
+(define (quaternion-scale q scale)
+  (quaternion
+   (* scale (a q))
+   (* scale (i q))
+   (* scale (j q))
+   (* scale (k q))))
+
+(: quaternion-sum (quaternion -> Float))
+(define (quaternion-sum q)
+  (+ (a q) (i q) (j q) (k q)))
+
+(: quaternion-conjugate (quaternion -> quaternion))
+(define (quaternion-conjugate q)
+  (quaternion (a q)
+              (- (i q))
+              (- (j q))
+              (- (k q))))
+
+(: quaternion-row-sum (quaternion quaternion -> Float))
+(define (quaternion-row-sum q r)
+  (quaternion-sum
+   (quaternion-conjugate
+    (quaternion-map2 * q r))))
+
+(: quaternion-single-product (quaternion quaternion -> quaternion))
+(define quaternion-single-product
+  (let ([a : quaternion-index-vector (vector a a a)] 
+        [b : quaternion-index-vector (vector i j k)]
+        [c : quaternion-index-vector (vector j k i)]
+        [d : quaternion-index-vector (vector k i j)])
+    (lambda ([q : quaternion]
+             [r : quaternion])
+      (fl->vector->quaternion 
+       (quaternion-row-sum q r)
+       (flvector3-sum (col q a r b)
+                      (col q c r d)
+                      (col q b r a)
+                      (flvector3-negative (col q d r c)))))))
 
 (: remap-to-vector (quaternion quaternion-index-vector -> FlVector))
 (define (remap-to-vector q m)
--- package/vraid/math/quaternion.rkt
+++ package/vraid/math/quaternion.rkt
@@ -37,26 +37,6 @@
     (axis-angle->quaternion (flvector3-cross-product u v)
                             (flvector3-angle u v))))
 
-(: flvector->quaternion (FlVector -> quaternion))
-(define (flvector->quaternion v)
-  (quaternion
-   (flvector-ref v 0)
-   (flvector-ref v 1)
-   (flvector-ref v 2)
-   (flvector-ref v 3)))
-
-(: quaternion->flvector (quaternion -> FlVector))
-(define (quaternion->flvector q)
-  (flvector (a q) (i q) (j q) (k q)))
-
-(: fl->vector->quaternion (Float FlVector -> quaternion))
-(define (fl->vector->quaternion f v)
-  (quaternion
-   f
-   (flvector-ref v 0)
-   (flvector-ref v 1)
-   (flvector-ref v 2)))
-
 (: quaternion-vector (quaternion -> FlVector))
 (define (quaternion-vector q)
   (flvector (i q)
@@ -69,32 +49,15 @@
    (flcos (fl* 0.5 a))
    (flvector3-scale (flsin (fl* 0.5 a)) (flvector3-normal v))))
 
-(: quaternion-sum (quaternion -> Float))
-(define (quaternion-sum q)
-  (+ (a q) (i q) (j q) (k q)))
-
-(: quaternion-conjugate (quaternion -> quaternion))
-(define (quaternion-conjugate q)
-  (quaternion (a q)
-              (- (i q))
-              (- (j q))
-              (- (k q))))
-
 (: quaternion-length-square (quaternion -> Float))
 (define (quaternion-length-square q)
-  (flvector-sum (flvector-sqr (quaternion->flvector q))))
+  (define square (lambda ([a : Float])
+                   (* a a)))
+  (quaternion-sum (quaternion-map square q)))
 
 (: quaternion-length (quaternion -> Float))
 (define (quaternion-length q)
   (flsqrt (quaternion-length-square q)))
-
-(: quaternion-scale (quaternion Float -> quaternion))
-(define (quaternion-scale q scale)
-  (quaternion
-   (* scale (a q))
-   (* scale (i q))
-   (* scale (j q))
-   (* scale (k q))))
 
 (: quaternion-inverse (quaternion -> quaternion))
 (define (quaternion-inverse q)
@@ -106,36 +69,6 @@
   (if (zero? (quaternion-length q))
       q
       (quaternion-scale q (/ (quaternion-length q)))))
-
-(: vector->quaternion (FlVector -> quaternion))
-(define (vector->quaternion v)
-  (fl->vector->quaternion 0.0 v))
-
-(: quaternion-row-sum (quaternion quaternion -> Float))
-(define (quaternion-row-sum q r)
-  (: m (Float Float * -> Float))
-  (define (m a . n) (foldl * a n))
-  (quaternion-sum
-   (quaternion-conjugate
-    (flvector->quaternion
-     (flvector-map m
-                   (quaternion->flvector q)
-                   (quaternion->flvector r))))))
-
-(: quaternion-single-product (quaternion quaternion -> quaternion))
-(define quaternion-single-product
-  (let ([a : quaternion-index-vector (vector a a a)] 
-        [b : quaternion-index-vector (vector i j k)]
-        [c : quaternion-index-vector (vector j k i)]
-        [d : quaternion-index-vector (vector k i j)])
-    (lambda ([q : quaternion]
-             [r : quaternion])
-      (fl->vector->quaternion 
-       (quaternion-row-sum q r)
-       (flvector3-sum (col q a r b)
-                      (col q c r d)
-                      (col q b r a)
-                      (flvector3-negative (col q d r c)))))))
 
 (: quaternion-product (quaternion * -> quaternion))
 (define (quaternion-product . quats)
