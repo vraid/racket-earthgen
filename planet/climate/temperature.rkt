358e310bcbc7bb239814ac484cb8f19a97eb4295
Tobias Grelsson <tgrelsson@gmail.com>
2018-02-18 20:19:49 +0100

    redefine below freezing as <= freezing point

--- planet/climate/temperature.rkt
+++ planet/climate/temperature.rkt
@@ -7,13 +7,12 @@
 (: freezing-temperature Float)
 (define freezing-temperature 273.15)
 
-(: above-freezing-temperature? (Float -> Boolean))
-(define (above-freezing-temperature? temperature)
-  (fl> temperature freezing-temperature))
-
 (: below-freezing-temperature? (Float -> Boolean))
 (define (below-freezing-temperature? temperature)
-  (fl< temperature freezing-temperature))
+  (fl<= temperature freezing-temperature))
+
+(define above-freezing-temperature?
+  (compose not below-freezing-temperature?))
 
 (: temperature-lapse-rate Float)
 (define temperature-lapse-rate 9.8e-3)
